f667ca9c30f39e0d086ebe4302aaf17de31f4b95
若寒若冰 <5433708@qq.com>
2024-03-16 17:29:28 +0800

    修改判断

--- js/utils.js
+++ js/utils.js
@@ -11,12 +11,27 @@ StringBuffer.prototype.toString = function () {
 }
 
 function createMenu() {
-    var allPages=[
-        {"menuName":"FIB-RW阅读下拉","url":"https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fibrw.html?access_token=2fd4d53480c117fa597505cebeceee9d"},
-        {"menuName":"FIB-R阅读拖拽","url":"https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fibr.html?access_token=2fd4d53480c117fa597505cebeceee9d"},
-        {"menuName":"WFD","url":"https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fireflywfd.html?access_token=2fd4d53480c117fa597505cebeceee9d"},
-        {"menuName":"SST","url":"https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fireflysst.html?access_token=2fd4d53480c117fa597505cebeceee9d"},
-        {"menuName":"生成PDF","url":"https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/createpdf.html?access_token=2fd4d53480c117fa597505cebeceee9d"}
+    var allPages = [
+        {
+            "menuName": "FIB-RW阅读下拉",
+            "url": "https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fibrw.html?access_token=2fd4d53480c117fa597505cebeceee9d"
+        },
+        {
+            "menuName": "FIB-R阅读拖拽",
+            "url": "https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fibr.html?access_token=2fd4d53480c117fa597505cebeceee9d"
+        },
+        {
+            "menuName": "WFD",
+            "url": "https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fireflywfd.html?access_token=2fd4d53480c117fa597505cebeceee9d"
+        },
+        {
+            "menuName": "SST",
+            "url": "https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fireflysst.html?access_token=2fd4d53480c117fa597505cebeceee9d"
+        },
+        {
+            "menuName": "生成PDF",
+            "url": "https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/createpdf.html?access_token=2fd4d53480c117fa597505cebeceee9d"
+        }
 
     ]
     var str = "";
@@ -33,11 +48,67 @@ function createMenu() {
 }
 
 function getAlljs() {
-    var js=[
+    var js = [
         "https://gitee.com/jackiechan/ptepractise/raw/main/js/fibrwprocess.js",
         "https://gitee.com/jackiechan/ptepractise/raw/main/js/fibrprocess.js",
         "https://gitee.com/jackiechan/ptepractise/raw/main/js/fireflywfd.js",
         "https://gitee.com/jackiechan/ptepractise/raw/main/js/fireflysst.js"
     ]
     return js;
+}
+
+/**
+ *
+ * @param key
+ * @param qNum
+ * @param type 是fibrw还是fibr还是其他
+ */
+function add2LocalStorage(key, qNum, type) {
+    $.localStorage.set(qNum + type, key);//根据题号保存,方便检查题目在不在
+    var data = $.localStorage.get(key);
+    if (!data) {
+        data = {"nums": [qNum]};
+    } else {
+        data.nums.push(qNum);
+    }
+    $.localStorage.set(key, data);
+}
+
+function removeFromLocalStorage(key, qNum, type) {
+    var data = $.localStorage.get(key);
+    if (data) {
+        var array = data.nums;
+        const index = array.indexOf(qNum); // 找到要删除的元素的索引
+        if (index !== -1) {
+            array.splice(index, 1); // 删除题号
+            data.nums = array;
+            $.localStorage.set(key, data)
+        }
+    }
+    //删除题号的映射关系
+    $.localStorage.remove(qNum + type);
+}
+
+function clearLocalStorageByKey(key, type) {
+    var data = $.localStorage.get(key);
+    if (data) {
+        var nums = data.nums;
+        nums.forEach(function (qNum) {
+            //删除所有的题号对应的位置
+            $.localStorage.remove(qNum + type);
+        });
+    }
+    $.localStorage.remove(key);
+}
+
+function getFromLocalStorage(key) {
+    return $.localStorage.get(key);
+}
+
+/**
+ * 检查某个题号在不在localstorage,注意题哈需要添加类型区分,避免不同类型题目题号一样,value是这个题号在另外一个localstorage中的key
+ * @param qNum
+ */
+function containsValue(qNum, type) {
+    return $.localStorage.get(qNum + type);
 }
