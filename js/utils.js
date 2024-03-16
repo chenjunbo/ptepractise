e7cd54be228fd8a38c7d7b2802967f164d96634b
若寒若冰 <5433708@qq.com>
2024-03-16 14:56:00 +0800

    change method

--- js/utils.js
+++ js/utils.js
@@ -9,3 +9,25 @@ StringBuffer.prototype.append = function (str) {
 StringBuffer.prototype.toString = function () {
     return this.__strings__.join("");
 }
+
+function createMenu() {
+    var allPages=[
+        {"menuName":"FIB-RW阅读下拉","url":"https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fibrw.html?access_token=2fd4d53480c117fa597505cebeceee9d"},
+        {"menuName":"FIB-R阅读拖拽","url":"https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fibr.html?access_token=2fd4d53480c117fa597505cebeceee9d"},
+        {"menuName":"WFD","url":"https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fireflywfd.html?access_token=2fd4d53480c117fa597505cebeceee9d"},
+        {"menuName":"SST","url":"https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/fireflysst.html?access_token=2fd4d53480c117fa597505cebeceee9d"},
+        {"menuName":"生成PDF","url":"https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/createpdf.html?access_token=2fd4d53480c117fa597505cebeceee9d"}
+
+    ]
+
+    allPages.forEach(function (obj) {
+        str += '<li class="layui-nav-item layui-nav-itemed">';
+        //拼接每一个地址
+        str += '<a class="" href="javascript:;" onclick="openRight(\'' + obj.url + '\')">' + obj.menuName + '</a>';
+        // if (obj.children.length > 0) {
+        //     makeMenu(obj.children)
+        // }
+        str += '</li>'
+    })
+    $("#left-menu").html(str);
+}
