67ba14a99ad5e23486f74fbd745d83ba886091de
若寒若冰 <5433708@qq.com>
2024-03-18 16:34:34 +0800

    rspdf

--- js/fireflyrs.js
+++ js/fireflyrs.js
@@ -1,5 +1,5 @@
 var fireflyrscontent;
-function fireFlySSTInit() {
+function fireFlyRSInit() {
     $.get("https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/data/fireflyrs.txt?access_token=c87299575627265144b7db286d3bf673", function (response) {
         fireflyrscontent=decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(response.content)))))))));
     })
