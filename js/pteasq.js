05e96abcad21d671bd56e77e6211dbc65e15b083
若寒若冰 <5433708@qq.com>
2024-03-18 22:23:03 +0800

    ASQ pdf

--- js/pteasq.js
+++ js/pteasq.js
@@ -2,7 +2,7 @@ var fireflyasqcontent;
 
 function fireFlyASQInit() {
     $.get("https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/data/fireflyasq.txt?access_token=c87299575627265144b7db286d3bf673", function (response) {
-        fireflyrscontent=decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(response.content)))))))));
+        fireflyasqcontent=decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(response.content)))))))));
     })
 }
 
