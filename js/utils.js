05474e63562fc9e15c9f4694b1f100c4fca34673
若寒若冰 <5433708@qq.com>
2024-03-22 03:10:38 +0800

    对错记录

--- js/utils.js
+++ js/utils.js
@@ -83,11 +83,19 @@ function createMenu() {
 
 function loadNotice() {
     $("#main").load("https://gitee.com/api/v5/repos/jackiechan/ptepractise/raw/notice.html?access_token=c87299575627265144b7db286d3bf673")
-    layer.confirm('我会阅读首页更新日志,获取更新的功能', {icon: 3}, function () {
+    var version="1";
+    var currentversion = window.localStorage.getItem("currentversion");
+    if (version == currentversion) {
         layer.msg('祝你八炸九炸心想事成', {icon: 0}, function () {
         });
-    }, function () {
-    });
+    }else{
+        layer.confirm('功能发生变化,请看更新日志', {icon: 3}, function () {
+            layer.msg('祝你八炸九炸心想事成', {icon: 0}, function () {
+            });
+            window.localStorage.setItem("currentversion", version);
+        }, function () {
+        });
+    }
 }
 
 function initOthers() {
