0297da9671c16fbf96b10d8f415a0e383f908eee
若寒若冰 <5433708@qq.com>
2024-03-22 17:08:18 +0800

    fibr代码整理

--- js/fibrprocess.js
+++ js/fibrprocess.js
@@ -549,7 +549,10 @@ function checkfibranswerbyfibrwmodel(obj, event, fibrdata,form) {
         if (!answer || !ans.endsWith(answer)) {//如果没有答案或者答案的值并不是select的结尾则认为是错的, 我们在生成数据的时候option的value就是代表这是第几个select的答案,select的name的结尾代表当前是第几个select
             // $(allSelects[i]).addClass("layui-form-danger");
             $(select).removeClass();
-            $(select).attr("class", "layui-form-danger");
+            var attr = $(select).attr("style");
+            attr = "border: 1px solid red;" + attr;
+            $(select).attr("style", attr);
+            // $(select).attr("class", "layui-form-danger");
             form.render();
             isWrong = true;
         }
