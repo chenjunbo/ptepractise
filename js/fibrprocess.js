d14293a1b965b3be1a80a8506b92b2af5e4a67bf
若寒若冰 <5433708@qq.com>
2024-03-22 15:33:08 +0800

    fibr代码整理

--- js/fibrprocess.js
+++ js/fibrprocess.js
@@ -211,7 +211,7 @@ function fibrTranslateDataFibrwModel(fibrData) {
     var num = fibrData.num;
     var text = fibrData.text;
     var choices = fibrData.choices.allData;
-    var title = "<div class=\"layui-form-item\"><label class=\"layui-form-label\" style=\"white-space:nowrap\">第" + (index + 1) + "题/共" + (currentList.length) + "题, 题号:" + num + "&nbsp;&nbsp;" + nameWithoutNum + "</label></div>"
+    var title = "<div class=\"layui-form-item\"><label class=\"layui-form-label\" style=\"white-space:nowrap\">第" + (index + 1) + "题/共" + (fibrCurrentList.length) + "题, 题号:" + num + "&nbsp;&nbsp;" + nameWithoutNum + "</label></div>"
     var selectionList = new Array();
     for (var key in choices) {
         var choice = choices[key];
