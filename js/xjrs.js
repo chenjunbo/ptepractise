e9cb2659a073399c40acbebfaf91c92820aee3a4
若寒若冰 <5433708@qq.com>
2024-03-26 17:12:08 +0800

    仅未做

--- js/xjrs.js
+++ js/xjrs.js
@@ -1,10 +1,13 @@
-var xjrsweekconten,xjrsmonthconten;
+var xjrsweekconten,xjrsmonthconten,xjrsallconten;
 function xjrsInit() {
     $.get("https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/data/rs/xjrsweek.txt?access_token=c87299575627265144b7db286d3bf673", function (response) {
         xjrsweekconten=decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(response.content)))))))));
     })
     $.get("https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/data/rs/xjrsmonth.txt?access_token=c87299575627265144b7db286d3bf673", function (response) {
         xjrsmonthconten=decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(response.content)))))))));
+    })
+    $.get("https://gitee.com/api/v5/repos/jackiechan/ptepractise/contents/data/rs/xjrsall.txt?access_token=c87299575627265144b7db286d3bf673", function (response) {
+        xjrsallconten=decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(response.content)))))))));
     })
 }
 
@@ -13,4 +16,8 @@ function currentxjrsweekconten() {
 }
 function currentxjrsmonthconten() {
     return xjrsmonthconten;
+}
+
+function currentxjrsallcontent() {
+    return xjrsallconten;
 }
