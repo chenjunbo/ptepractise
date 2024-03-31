120c2b9cef5b7d37364cbda4c8ca9b838115109b
若寒若冰 <5433708@qq.com>
2024-03-31 16:32:21 +0800

    添加重置区域

--- js/utils.js
+++ js/utils.js
@@ -175,7 +175,7 @@ function openRightNew(url) {
 
 function loadNotice() {
     $("#main").load("https://gitee.com/api/v5/repos/jackiechan/ptepractise/raw/notice.html?access_token=c87299575627265144b7db286d3bf673")
-    var version="16";
+    var version="17";
     var currentversion = window.localStorage.getItem("currentversion");
     if (version == currentversion) {
         layer.msg('祝你八炸九炸心想事成', {icon: 0}, function () {
