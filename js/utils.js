b152c6167af55b6388acd3f2a0ea5d094bab7844
若寒若冰 <5433708@qq.com>
2024-04-20 21:20:59 +1200

    复制mp3下载地址

--- js/utils.js
+++ js/utils.js
@@ -191,14 +191,14 @@ function loadNotice() {
         $("#main").load("https://gitee.com/api/v5/repos/jackiechan/ptepractise/raw/notice.html?access_token=c87299575627265144b7db286d3bf673&ref=webversion")
     }
     // $("#main").load("https://gitee.com/api/v5/repos/jackiechan/ptepractise/raw/notice.html?access_token=c87299575627265144b7db286d3bf673&ref=webversion")
-    var version="4.19.1";
+    var version="4.20.1";
     var currentversion = window.localStorage.getItem("currentversion");
     if (version == currentversion) {
         layer.msg('祝你八炸九炸心想事成', {icon: 0}, function () {
         });
     }else{
-        // layer.confirm('功能发生变化,请看更新日志', {icon: 3}, function () {
-        layer.confirm('今天(19日)飞新西兰,20日周六下午达到,期间的更新会延迟,到达后那边整理下再更新', {icon: 3}, function () {
+        layer.confirm('功能发生变化,请看更新日志', {icon: 3}, function () {
+        // layer.confirm('今天(19日)飞新西兰,20日周六下午达到,期间的更新会延迟,到达后那边整理下再更新', {icon: 3}, function () {
             layer.msg('祝你八炸九炸心想事成', {icon: 0}, function () {
             });
             window.localStorage.setItem("currentversion", version);
