32c7fe0493e9fcbf0a3b9dbaa701d6dc872baef2
若寒若冰 <5433708@qq.com>
2024-04-26 14:24:48 +1200

    增加C哥SST预测跳转到猩际功能

--- js/fireflysst.js
+++ js/fireflysst.js
@@ -3,18 +3,20 @@ let sstAllDataList;
 const sstMap = new Map();
 var sstIndex = 0;//当前第几条
 sstlocalstoragetype = "fireflysst";
+isXJSST = false;
+
 function fireFlySSTInit() {
-    $.get(getGitContentPre()+"/data/sst/fireflysst.txt"+getGitContentAccess(), function (response) {
+    $.get(getGitContentPre() + "/data/sst/fireflysst.txt" + getGitContentAccess(), function (response) {
         var result
         try {
             result = decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(response.content)))))))));
-        } catch (e){
+        } catch (e) {
             result = decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(response))))));
         }
-        sstAllDataList= JSON.parse(result);
+        sstAllDataList = JSON.parse(result);
         for (let i = 0; i < sstAllDataList.length; i++) {
             var sstData = sstAllDataList[i];
-            sstMap.set(sstData.id+"", sstData);
+            sstMap.set(sstData.id + "", sstData);
         }
     })
 }
@@ -26,10 +28,11 @@ function fireflySSTCurrentTypedata(param) {
     sstIndex = 0;
     sstCurrentList = new Array();
     var localstoragedata;
+    isXJSST = false;
     switch (type) {
         case "1":
             //高频预测
-            filePath = getGitContentPre()+"/questions/sst/fireflysstprediction.txt"+getGitContentAccess()
+            filePath = getGitContentPre() + "/questions/sst/fireflysstprediction.txt" + getGitContentAccess()
             break;
         case "2":
 
@@ -41,6 +44,10 @@ function fireflySSTCurrentTypedata(param) {
                 localstoragedata = json.nums;
             }
             break;
+        case "-1":
+            filePath = getGitContentPre() + "/questions/sst/ce_sst_all.txt" + getGitContentAccess();
+            isXJSST = true;
+            break
 
     }
 
@@ -51,27 +58,30 @@ function fireflySSTCurrentTypedata(param) {
             let qNums;
             try {
                 qNums = decodeURIComponent(escape(window.atob(response.content))).split(/[(\r\n)\r\n]+/); // 根据换行或者回车进行识别
-            }catch (e) {
+            } catch (e) {
                 qNums = response.split(/[(\r\n)\r\n]+/);
             }
-            qNums.forEach((item, index) => { // 删除空项
-                if (qNum && qNum != item) {
+            if (isXJSST) {
+                sstCurrentList = qNums
+            } else {
+                qNums.forEach((item, index) => { // 删除空项
+                    if (qNum && qNum != item) {
 
-                } else {
-                    if (!item) {
-                        qNums.splice(index, 1);
                     } else {
-                        var sstData = sstMap.get(item+"");
-                        if (sstData) {
-                            sstCurrentList.push(sstData);
+                        if (!item) {
+                            qNums.splice(index, 1);
+                        } else {
+                            var sstData = sstMap.get(item + "");
+                            if (sstData) {
+                                sstCurrentList.push(sstData);
+                            }
                         }
                     }
-                }
-            })
-
+                });
+            }
         });
         $.ajaxSettings.async = true;
-    }else if (localstoragedata) {
+    } else if (localstoragedata) {
         localstoragedata.forEach((item, index) => { // 删除空项
             if (qNum && qNum != item) {
 
@@ -79,20 +89,20 @@ function fireflySSTCurrentTypedata(param) {
                 if (!item) {
                     localstoragedata.splice(index, 1);
                 } else {
-                    var sstData = sstMap.get(item+"");
+                    var sstData = sstMap.get(item + "");
                     if (sstData) {
                         sstCurrentList.push(sstData);
                     }
                 }
             }
         })
-    }else{
+    } else {
         if (qNum) {
-            var sstData = sstMap.get(qNum+"");
+            var sstData = sstMap.get(qNum + "");
             if (sstData) {
                 sstCurrentList.push(sstData);
             }
-        }else{
+        } else {
             sstCurrentList = sstAllDataList;
         }
     }
@@ -103,47 +113,52 @@ function fireflySSTCurrentTypedata(param) {
 
 }
 
-function fireFlySSTTranslateData(fireflySSTData,params){
-    var num = fireflySSTData.id;
-    var name = fireflySSTData.title;
-    var text = "";
-    var title = "<div class=\"layui-form-item\"><div class=\"layui-inline\"><label  style=\"white-space:nowrap\">第" + (sstIndex + 1) + "题/共" + (sstCurrentList.length) + "题, 题号:" + num + "&nbsp;&nbsp;" + name +  "&nbsp;&nbsp;</label><div class=\"layui-inline\"><span style=\"color: red\" id=\"timer\"></span></div></div></div>"
-    text = text + title;
-    var simpleanswer = params.simpleanswer;
-    var keyword = params.keyword;
-    var chinese = params.chinese;
-    var logicpic = params.logicpic;
-    if (simpleanswer) {
-        text = text + "简单答案:"+ "<br>"  + fireflySSTData.simpleAnswer;
-    }
-    if (keyword) {
-        text = text + "<br>关键词:"+ "<br>"  + fireflySSTData.keyWords;
-    }
-    if (chinese) {
-        text = text + "<br>中文速记:" + "<br>" + "<span style=\"font-size:16px;color:red;\">"+fireflySSTData.chineseContent+"</span>";
-    }
-    if (logicpic) {
-        var sstPic = fireflySSTData.sstPic;
-        if (!sstPic) {
-            sstPic="暂无梳理图"
-            text = text + "<br>逻辑梳理图:" + "<br>" + "<span style=\"font-size:16px;color:red;\">暂无梳理图</span>";
-
-        }else{
-            text = text +'<br>逻辑梳理图:<br><img src='+sstPic+'  width="40%">';
+function fireFlySSTTranslateData(fireflySSTData, params) {
+    if (isXJSST) {
+        startTimer();
+        return "<div class=\"layui-form-item\"><div class=\"layui-inline\"><label  style=\"white-space:nowrap\">第" + (sstIndex + 1) + "题/共" + (sstCurrentList.length) + "题, 题号:" + fireflySSTData + "&nbsp;&nbsp;本分类主要用于C哥预测跳转到⭐️&nbsp;&nbsp;</label><div class=\"layui-inline\"><span style=\"color: red\" id=\"timer\"></span></div></div></div>"
+    } else {
+        var num = fireflySSTData.id;
+        var name = fireflySSTData.title;
+        var text = "";
+        var title = "<div class=\"layui-form-item\"><div class=\"layui-inline\"><label  style=\"white-space:nowrap\">第" + (sstIndex + 1) + "题/共" + (sstCurrentList.length) + "题, 题号:" + num + "&nbsp;&nbsp;" + name + "&nbsp;&nbsp;</label><div class=\"layui-inline\"><span style=\"color: red\" id=\"timer\"></span></div></div></div>"
+        text = text + title;
+        var simpleanswer = params.simpleanswer;
+        var keyword = params.keyword;
+        var chinese = params.chinese;
+        var logicpic = params.logicpic;
+        if (simpleanswer) {
+            text = text + "简单答案:" + "<br>" + fireflySSTData.simpleAnswer;
         }
+        if (keyword) {
+            text = text + "<br>关键词:" + "<br>" + fireflySSTData.keyWords;
+        }
+        if (chinese) {
+            text = text + "<br>中文速记:" + "<br>" + "<span style=\"font-size:16px;color:red;\">" + fireflySSTData.chineseContent + "</span>";
+        }
+        if (logicpic) {
+            var sstPic = fireflySSTData.sstPic;
+            if (!sstPic) {
+                sstPic = "暂无梳理图"
+                text = text + "<br>逻辑梳理图:" + "<br>" + "<span style=\"font-size:16px;color:red;\">暂无梳理图</span>";
+
+            } else {
+                text = text + '<br>逻辑梳理图:<br><img src=' + sstPic + '  width="40%">';
+            }
+        }
+        startTimer();
+        return text;
+
     }
-    startTimer();
-    return text;
 }
 
 function createFireFlySSTPdfHtml(params, serNum, fireflySSTData) {
-
     var num = fireflySSTData.id;
     var name = fireflySSTData.title;
     var questionDiv = document.createElement("div");
     $(questionDiv).attr("style", "padding-left: 20px;padding-right: 20px;line-height: 30px;font-size: larger");
     var h3 = document.createElement("h3");
-    h3.innerHTML = serNum + "." + "&nbsp;" + "&nbsp;" + name + "&nbsp;" + "&nbsp;题号:" + num + "<br/>" ;
+    h3.innerHTML = serNum + "." + "&nbsp;" + "&nbsp;" + name + "&nbsp;" + "&nbsp;题号:" + num + "<br/>";
     $(questionDiv).append(h3);
     var text = "";
     var simpleanswer = params.simpleanswer;
@@ -151,32 +166,209 @@ function createFireFlySSTPdfHtml(params, serNum, fireflySSTData) {
     var chinese = params.chinese;
     var logicpic = params.logicpic;
     if (simpleanswer) {
-        text = text + "简单答案:"+ "<br>"  + fireflySSTData.simpleAnswer;
+        text = text + "简单答案:" + "<br>" + fireflySSTData.simpleAnswer;
     }
     if (keyword) {
-        text = text + "<br>关键词:"+ "<br>"  + fireflySSTData.keyWords+ "<br>";
+        text = text + "<br>关键词:" + "<br>" + fireflySSTData.keyWords + "<br>";
     }
     if (chinese) {
-        text = text + "<br>中文速记:" + "<br>" + "<span style=\"font-size:16px;color:red;\">"+fireflySSTData.chineseContent+"</span>";
+        text = text + "<br>中文速记:" + "<br>" + "<span style=\"font-size:16px;color:red;\">" + fireflySSTData.chineseContent + "</span>";
     }
     if (logicpic) {
         var sstPic = fireflySSTData.sstPic;
         if (!sstPic) {
-            sstPic="暂无梳理图"
+            sstPic = "暂无梳理图"
             text = text + "<br>逻辑梳理图:" + "<br>" + "<span style=\"font-size:16px;color:red;\">暂无梳理图</span>";
 
-        }else{
-            text = text +'<br>逻辑梳理图:<br><img src='+sstPic+' width="40%">';
+        } else {
+            text = text + '<br>逻辑梳理图:<br><img src=' + sstPic + ' width="40%">';
         }
     }
-    text = text + "<br/>"+ "<br/>";
+    text = text + "<br/>" + "<br/>";
 
     $(questionDiv).append(text);
     return questionDiv;
 }
 
+function fireflysstsearch() {
+    $("#pre").hide();
+    var params = $("#fireflysst-form").serializeJson();
+    fireflySSTCurrentTypedata(params);
+    var fireFlySSTData = currentSSTData();
+    if (!fireFlySSTData) {
+        $("#question-form").hide();
+        layer.msg('当前分类下不存在该题目', {icon: 0}, function () {
+            // layer.msg('提示框关闭后的回调');
+        });
+        return false;
+    } else {
+        $("#question-form").show();
+    }
+    var content = fireFlySSTTranslateData(fireFlySSTData, params);
+    $("#question-div").html(content);
+    if (isSSTLast()) {
+        $("#next").hide();
+    } else {
+        $("#next").show();
+    }
+    if (getSSTTotalNum() == 1) {
+        $("#gotoarea").hide();
+    } else {
+        $("#gotoarea").show();
+    }
+    if (isXJSST) {
+        checkFav(fireFlySSTData, sstlocalstoragetype);
+    } else {
 
+        checkFav(fireFlySSTData.id, sstlocalstoragetype);
+    }
+}
 
+function sstNext() {
+    if (isSSTLast()) {
+        $("#next").hide();
+        return false;
+    }
+    var params = $("#fireflysst-form").serializeJson();
+    var fireFlySSTData = sstNextQuest();
+    var content = fireFlySSTTranslateData(fireFlySSTData, params);
+    if (isSSTLast()) {
+        $("#next").hide();
+    }
+    $("#question-div").html(content);
+    if (!isSSTFirst()) {
+        $("#pre").show();
+    }
+    if (isXJSST) {
+        checkFav(fireFlySSTData, sstlocalstoragetype);
+    } else {
+
+        checkFav(fireFlySSTData.id, sstlocalstoragetype);
+    }
+}
+
+function sstPre() {
+    if (isSSTFirst()) {
+        $("#pre").hide();
+        return false;
+    }
+    var params = $("#fireflysst-form").serializeJson();
+    // var content = fireFlyPreQuest();
+    var fireFlySSTData = sstPreQuest();
+    var content = fireFlySSTTranslateData(fireFlySSTData, params);
+    if (isSSTFirst()) {
+        $("#pre").hide();
+    }
+    if (!isSSTLast()) {
+        $("#next").show();
+    }
+    $("#question-div").html(content);
+    if (isXJSST) {
+        checkFav(fireFlySSTData, sstlocalstoragetype);
+    } else {
+
+        checkFav(fireFlySSTData.id, sstlocalstoragetype);
+    }
+}
+
+function sstgotoindex() {
+    var qIndex = $("#qindex").val();//想要跳转的题目
+    console.log(qIndex);
+    if (!qIndex || qIndex <= 0 || qIndex > getSSTTotalNum) {
+
+    } else {
+        setSSTIndex(parseInt(qIndex));
+        var fireFlySSTData = currentSSTData();
+        if (!fireFlySSTData) {
+            layer.msg('超出题目数量范围', {icon: 0}, function () {
+            });
+            return false;
+        }
+        var params = $("#fireflysst-form").serializeJson();
+        var content = fireFlySSTTranslateData(fireFlySSTData, params);
+        if (isSSTFirst()) {
+            $("#pre").hide();
+        } else {
+            $("#pre").show();
+        }
+        if (!isSSTLast()) {
+            $("#next").show();
+        } else {
+            $("#next").hide();
+        }
+        $("#question-div").html(content);
+        if (isXJSST) {
+            checkFav(fireFlySSTData, sstlocalstoragetype);
+        } else {
+
+            checkFav(fireFlySSTData.id, sstlocalstoragetype);
+        }
+    }
+}
+
+function adddeletesstfav() {
+    var fireFlySSTData = currentSSTData();
+    if (!fireFlySSTData) {
+        layer.msg('题目无效', {icon: 0}, function () {
+        });
+        return false;
+    }
+    if (isXJSST) {
+        var isContains = containsValue(fireFlySSTData, sstlocalstoragetype);
+        if (isContains) {
+            layer.confirm('是否删除收藏？', {icon: 3}, function () {
+                removeFavFromLocalStorage(fireFlySSTData, sstlocalstoragetype);
+                layer.msg('操作完成', {icon: 0}, function () {
+                });
+                checkFav(fireFlySSTData, sstlocalstoragetype);
+            }, function () {
+            });
+
+        } else {
+            layer.confirm('是否添加到收藏？', {icon: 3}, function () {
+                add2LocalStorage("fireflysst", fireFlySSTData, sstlocalstoragetype)
+                layer.msg('操作完成', {icon: 0}, function () {
+                });
+                checkFav(fireFlySSTData, sstlocalstoragetype);
+            }, function () {
+            });
+
+        }
+    } else {
+        var isContains = containsValue(fireFlySSTData.id, sstlocalstoragetype);
+        if (isContains) {
+            layer.confirm('是否删除收藏？', {icon: 3}, function () {
+                removeFavFromLocalStorage(fireFlySSTData.id, sstlocalstoragetype);
+                layer.msg('操作完成', {icon: 0}, function () {
+                });
+                checkFav(fireFlySSTData.id, sstlocalstoragetype);
+            }, function () {
+            });
+
+        } else {
+            layer.confirm('是否添加到收藏？', {icon: 3}, function () {
+                add2LocalStorage("fireflysst", fireFlySSTData.id, sstlocalstoragetype)
+                layer.msg('操作完成', {icon: 0}, function () {
+                });
+                checkFav(fireFlySSTData.id, sstlocalstoragetype);
+            }, function () {
+            });
+
+        }
+    }
+
+}
+
+function sstgotoaepui() {
+    if (isXJSST) {
+        var data = currentSSTData();
+        var local = "cn";
+        gotoxj(local, "ssts", data);
+    } else {
+        layer.msg('SST跳转暂时只针对⭐️题目,请选择⭐️分组', {icon: 0}, function () {
+        });
+    }
+}
 
 function sstNextQuest() {
     if (sstIndex < sstCurrentList.length - 1) {
@@ -209,8 +401,6 @@ function getSSTindex() {
 }
 
 
-
-
 function getSSTTotalNum() {
     return sstCurrentList.length;
 }
@@ -229,10 +419,10 @@ function currentSSTListData() {
 
 function cleanfireflysstfav() {
     layer.confirm('是否清空收藏夹？', {icon: 3}, function () {
-        cleanFav(sstlocalstoragetype,sstlocalstoragetype)
+        cleanFav(sstlocalstoragetype, sstlocalstoragetype)
         layer.msg('操作完成', {icon: 0}, function () {
         });
-        checkFav(currentSSTData().id, localStorageType);
+        checkFav(currentSSTData().id, sstlocalstoragetype);
     }, function () {
     });
 
