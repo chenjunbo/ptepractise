6f5f05b64f908af8a56e59fc21b567f52e70db40
若寒若冰 <5433708@qq.com>
2024-04-26 15:30:35 +1200

    wfd难度句

--- js/fireflywfd.js
+++ js/fireflywfd.js
@@ -220,7 +220,7 @@ function loadwfdpdfmd(filename,pdfname){
             result = decodeURIComponent(escape(window.atob(decodeURIComponent(escape(window.atob(response))))));
         }
         var rawPre = getRawPre();
-        str.replace(new RegExp("https://gitee.com/jackiechan/ptepractise/raw/webversion","gm"),rawPre)
+        result= result.replace(new RegExp("https://gitee.com/jackiechan/ptepractise/raw/webversion","gm"),rawPre)
         var newPage = document.implementation.createHTMLDocument('New Page');
         var name = pdfname;
         var body = newPage.body;
