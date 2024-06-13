af5396452e94ee611a58abea2a19325b6aafccd7
陈俊波 <5433708@qq.com>
2024-06-13 16:14:42 +1200

    ra高频词

--- js/fireflywfd.js
+++ js/fireflywfd.js
@@ -7,7 +7,7 @@ var localStorageType = "fireflywfd";
 var plaintxt,ankitxt;
 var fireFlyWFDHtmlMap = null  //key为题号,value为html格式的内容字符串
 var fireFlyWFDCurrentHtmlList
-var excWords = ["of", "to", "as", "at", "on", "in", "for", "by", "about", "with", "up", "a", "an", "the", "this", "that", "is", "are", "was", "were", "has", "have", "had", "been", "be", "can", "could", "would", "should", "I", "you", "he", "she", "his", "her", "your", "and", "or"]
+var excWords = ["it","their","from","of", "to", "as", "at", "on", "in", "for", "by", "about", "with", "up", "a", "an", "the", "this", "that", "is", "are", "was", "were", "has", "have", "had", "been", "be", "can", "could", "would", "should", "I", "you", "he", "she", "his", "her", "your", "and", "or","they","more","but","our","which","not","people","we","we","also","one","new","these","than","when","such","will","how","many","may","most","into","other","all","some","its","who","study","there","if","what","while","water","resea","over","so","like","years","year","time","them","often","use","used","us","do","out","now","where","two","one","yes","no","it's",]
 
 function fireFlyWFDInit(form) {
     $.get(getGitContentPre() + "/data/wfd/fireflywfd.txt" + getGitContentAccess(), function (response) {
