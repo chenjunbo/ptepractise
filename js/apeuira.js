c3f50da3149736a9b92ab75a2e12817ec9ced919
陈俊波 <5433708@qq.com>
2024-06-13 16:01:55 +1200

    ra高频词

--- js/apeuira.js
+++ js/apeuira.js
@@ -5,6 +5,7 @@ var xjraIdsSet = new Set();
 const xjraenMap = new Map();
 var raindex = 0;//当前第几条
 var xjralocalStorageType = "xjra";
+var excWords = ["of", "to", "as", "at", "on", "in", "for", "by", "about", "with", "up", "a", "an", "the", "this", "that", "is", "are", "was", "were", "has", "have", "had", "been", "be", "can", "could", "would", "should", "I", "you", "he", "she", "his", "her", "your", "and", "or"]
 
 function raInit() {
     $.ajax({
@@ -521,6 +522,63 @@ function currentRaListData() {
 }
 
 
+
+function create_ra_all_words_order_by_dic(order_by_count) {
+    var allWords = ""
+    var table = document.createElement("table");
+    var thead = document.createElement("thead");
+    var thead_tr = document.createElement("tr");
+    var word_th = document.createElement("th");
+    var count_th = document.createElement("th");
+    word_th.innerHTML = "单词"
+    count_th.innerHTML = "频率"
+    thead_tr.appendChild(word_th)
+    thead_tr.appendChild(count_th)
+    thead.appendChild(thead_tr)
+    table.appendChild(thead)
+
+    var tbody = document.createElement("tbody");
+    raenList.forEach((ra, ind) => {
+        allWords += (ra.text.toLowerCase().replace(".", "").replace(",", "")) + " "
+    })
+    let result = allWords.split(" ").reduce((temp, data) => {
+        if (excWords.indexOf(data) > -1 || !data) {
+
+        } else {
+            temp[data] = temp[data] ? temp[data] + 1 : 1;
+        }
+        return temp;
+    }, {})
+    var sorted_result_by_count = Object.keys(result).sort((key1, key2) => result[key1] - result[key2]);
+    var sorted_result_by_count_daoxu = Object.keys(result).sort((key1, key2) => result[key2] - result[key1]);
+    var sorted_result_by_char = Object.keys(result).sort((key1, key2) => {
+        let a = key1.toLowerCase();
+        let b = key2.toLowerCase();
+        if (a < b) return -1;
+        if (a > b) return 1;
+        return 0;
+    });
+    console.log(sorted_result_by_count)
+    console.log(sorted_result_by_char)
+    var array = sorted_result_by_char;
+    if (order_by_count==0) {
+        array = sorted_result_by_count;
+    }else if (order_by_count == 1) {
+        array = sorted_result_by_count_daoxu;
+    }
+    array.forEach((word, index) => {
+        var wordTd = document.createElement("td");
+        var tr = document.createElement("tr");
+        var countTd = document.createElement("td");
+        wordTd.innerHTML = word;
+        countTd.innerHTML = result[word];
+        tr.appendChild(wordTd)
+        tr.appendChild(countTd)
+        tbody.appendChild(tr)
+    });
+    table.appendChild(tbody)
+    return table;
+}
 
 
 function cleanxjrafav() {
