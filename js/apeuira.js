4481353b395461820581d208ac223a0c252c1d22
陈俊波 <5433708@qq.com>
2024-06-26 20:37:11 +1200

    高频过滤

--- js/apeuira.js
+++ js/apeuira.js
@@ -12,6 +12,7 @@ var excWords = ["it","their","from","of", "to", "as", "at", "on",
     "people","we","we","also","one","new","these","than","when","such","will","how","many","may",
     "most","into","other","all","some","its","who","study","there","if","what","while","water","reset",
     "age","add","after","before","afternoon","ago","air","am",
+    "students","i","next","help","week","need","my","work","first","take","any","think","too","end","every","him","four",
     "the","it","must","this","a","get","me","very","good","make","student","book","books","most","want","class","don't","our","your","many","lot","food","start","day","find","go","here","to",
     "over","so","like","years","year","time","them","often","use","used","us","do","out","now","where",
     "two","one","yes","no","it's","—"]
